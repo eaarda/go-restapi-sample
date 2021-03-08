f43f8b82711f02cb64c1d0df804a8b4f0210c55f
eaarda <elifaarda@gmail.com>
2021-03-08 23:57:37 +0300

    gin-gorm

--- main.go
+++ main.go
@@ -1,48 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
-type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-type Articles []Article
-
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
-	}
-
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
-}
-
-func testPostArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Test POST endpoint worked")
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Homepage Endpoint Hit")
-}
-
-func handleRequests() {
-
-	myRouter := mux.NewRouter().StrictSlash(true)
-
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
-	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", myRouter))
-}
-
 func main() {
-	handleRequests()
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "API Test",
+		})
+	})
+	r.Run(":5000")
 }
---
+++ models/book.go
@@ -0,0 +1,11 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type Book struct {
+	gorm.Model
+	ID     uint   `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Page   int    `json:"page"`
+}
